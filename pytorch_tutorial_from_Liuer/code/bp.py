b5f3a96ef6ce138b44826d2478d6073d88c74e80
sx-cpu <sunshixing71@gmail.com>
2025-09-20 11:08:35 +0800

    linear regression

---
+++ pytorch_tutorial_from_Liuer/code/bp.py
@@ -0,0 +1,34 @@
+# Introduce how to use basic function of tensor and computation graph.
+
+import torch
+
+
+x_data = [1.0, 2.0, 3.0]
+y_data = [2.0, 4.0, 6.0]
+
+w = torch.tensor([1.0])
+w.requires_grad = True
+
+def forward(x):
+    return x * w
+
+def loss(x, y):
+    y_pred = forward(x)
+    return (y_pred - y) ** 2
+
+
+print(f"predict (before training) 4 {forward(4).item()}")
+
+for epoch in range(100):
+    for x, y in zip(x_data, y_data):
+        l = loss(x, y)
+        l.backward()
+        print(f'\tgrad: {x} {y} {w.grad.item()}')
+        w.data = w.data - 0.01 * w.grad.data
+
+        w.grad.data.zero_()
+    
+    print(f'progress: {epoch} {l.item()}')
+
+print(f'predict (after training) 4 {forward(4).item()}')
+
